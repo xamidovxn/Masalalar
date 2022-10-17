2ae6d5ba442fe966093ad604f154099ee07fdcda
xamidovxn <abduxamidov017@gmail.com>
2022-10-17 11:49:33 +0500

    Masalalar

---
+++ script.js
@@ -0,0 +1,290 @@
+"use strict"
+
+// M-1
+
+// let l = prompt("Son kiriting")
+// console.log(l + " sm = " + l / 100 + " m");
+
+
+// M-2
+
+// let m = prompt("Son kiriting")
+// console.log(m + " kg = " + m / 1000 + " tonna");
+
+// M-3
+
+// M-4
+
+// let num = prompt("Ikki xonali son kiriting")
+// let Fnum = num / 10
+// let Snum = num % 10
+// console.log(num + " -> " + Math.floor(Fnum) + ", " + Math.floor(Snum));
+
+
+// M-5
+
+// let num = prompt("Ikki xonali son kiriting")
+// let Fnum = num / 10
+// let Snum = num % 10
+// console.log(num + " -> " + Math.floor(Fnum + Snum));
+
+
+// M-6
+
+// let num = prompt("Ikki xonali son kiriting")
+// let Fnum = Math.floor(num / 10)
+// let Snum = num % 10
+// console.log(num + " -> " + Fnum * Snum);
+
+
+// M-7
+
+// let num = prompt("999 dan katta son kiriting")
+// let count = num / 100
+// console.log(count);
+
+
+// M-8
+
+// let N = prompt("Secund kiriting") /* 1 min 60 sec.  1 soat 3600 sec.*/
+// let min = Math.floor(N / 60)
+// let hour = Math.floor(N / 3600)
+// console.log("Kun boshidan " + N + " secund " + min + " minut " + hour + " soat o'tdi");
+
+
+/* if else */
+
+
+// M-1
+
+// let num = prompt("Son kiriting")
+// if (num > 0) {
+//    console.log("Bu musbot son");
+// }
+// else {
+//    console.log("Bu manfiy son");
+// }
+
+
+// M-2
+
+// let num = prompt("Son kiriting")
+
+// if(num % 2 != 0) {
+//    console.log("Bu toq son");
+// }
+
+
+// M-3
+
+// let num = prompt("Son kiriting")
+// if(num % 2 == 0) {
+//    console.log("Bu juft son");
+// }
+
+
+// M-4
+
+// let a = 8;
+// let b = 0;
+// if (a > 0 && b > 0) {
+//    console.log("Butun son");
+// }
+// else {
+//    console.log("Butun son emas");
+// }
+
+
+// M-5
+
+// let a = 15;
+// let b = 10;
+// let c = 9;
+// if (a > b && b > c) {
+//    console.log("True");
+// }
+// else {
+//    console.log("False");
+// }
+
+
+// M-6
+
+// let a = 15;
+// let b = 5;
+// let c = 9;
+// if (a > b && b < c) {
+//    console.log("True");
+// }
+// else {
+//    console.log("False");
+// }
+
+
+// M-7
+
+// let a = prompt('a=')
+// let b = prompt('b=')
+// if (a % 2 != 0 && b % 2 != 0) {
+//    console.log('Toq son');
+// }
+// else {
+//    console.log('Toq son emas');
+// }
+
+
+// M-8
+
+// let a = 5;
+// let b = 5;
+// let c = 5;
+// let d = 5;
+// if (a > 0 && b > 0 && c > 0 && d > 0) {
+//    console.log('Musbat son');
+// }
+// else {
+//    console.log('Manfiy son');
+// }
+
+
+// M-9
+
+// let a = 5;
+// let b = -5;
+// let c = 5;
+// if (a > 0 && b > 0 || c > 0) {
+//    console.log('true');
+// }
+// else {
+//    console.log('false');
+// }
+
+
+// M-10
+
+// let search = prompt("son kiriting")
+
+// if (search == 1) {
+//    console.log('Dushanba');
+// }
+// else if (search == 2) {
+//    console.log('Seshanba');
+// }
+// else if (search == 3) {
+//    console.log('Chorshanba');
+// }
+// else if (search == 4) {
+//    console.log('Payshanba');
+// }
+// else if (search == 5) {
+//    console.log('Juma');
+// }
+// else if (search == 6) {
+//    console.log('Shanba');
+// }
+// else if (search == 7) {
+//    console.log('Yakshanba');
+// }
+// else {
+//    console.log("Bunday hafta kuni yo'q");
+// }
+
+
+// M-11
+
+// let code = prompt("Telefon nomer kodini kiriting")
+
+// if (code == 98) {
+//    console.log('Perfectum');
+// }
+// else if (code == 90) {
+//    console.log('Beeline');
+// }
+// else if (code == 99) {
+//    console.log('Uzmobile');
+// }
+// else {
+//    console.log('Bundat code kiritilmagan!');
+// }
+
+
+// M-12
+
+// let num = prompt("son kiriting")
+// if (num < 0) {
+//    let plus = +num + 1;
+//    console.log(plus);
+// }
+// else if (num > 0) {
+//    let minus = +num + 1;
+//    console.log(minus);
+// }
+
+
+// M-13
+
+// let num = prompt("son kiriting")
+// if (num < 0) {
+//    let plus = +num + 2;
+//    console.log(plus);
+// }
+// else if (num > 0) {
+//    let minus = +num + 3;
+//    console.log(minus);
+// }
+
+
+// M-14
+
+// let a = 8;
+// let b = 10;
+// if (a > b) {
+//    console.log(a);
+// }
+// else {
+//    console.log(b);
+// }
+
+
+// M-15
+
+// let a = 80;
+// let b = 100;
+// let c = 1;
+// let count;
+
+// if (a > b) {
+//    count = a;
+//    console.log(count);
+// }
+// else if (b > c) {
+//    count = b;
+//    console.log(count);
+// }
+// else {
+//    count = c;
+//    console.log(count);
+// }
+
+
+// M-16
+
+// let a = 8;
+// let b = 100;
+// let c = 10;
+// let count;
+
+// if (a < b && a < c) {
+//    count = a;
+//    console.log(count);
+// }
+// else if (b < a && b < c) {
+//    count = b;
+//    console.log(count);
+// }
+
+// else {
+//    count = c;
+//    console.log(count);
+// }
+
